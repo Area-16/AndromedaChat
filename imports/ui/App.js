a2709b5f7f3c2af7fcf325310f667b5b99820744
Douglas Alves <douglas.expalves@gmail.com>
2017-11-28 15:20:01 -0200

    app ui component and styles created

---
+++ imports/ui/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Chat from './Chat'
+
+class App extends Component {
+  render () {
+    return (
+      <Chat title='React Chat' />
+    )
+  }
+}
+
+export default App
