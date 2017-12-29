2efe3fdd4bfd9fdfc47cef55d5d80d5b4c51a482
Douglas Alves <douglas.expalves@gmail.com>
2017-12-29 20:12:42 -0200

    removed useless console.log's

--- imports/api/server.js
+++ imports/api/server.js
@@ -9,11 +9,9 @@ let serverBootstrap = () => {
   let socket = io(app)
 
   socket.on('connection', socketServer => {
-    console.log(socketServer.id)
     socketServer.on('SEND_MESSAGE', data => {
       data.time = new Date().getDate() + '/' + new Date().getMonth()
       data.timeframe = new Date().getHours() + ':' + new Date().getMinutes()
-      console.log(data)
       socket.emit('RECEIVE_MESSAGE', data)
     })
   })
