db157a69daaf330e3f77516c8a1eb84fa3820cc7
Douglas Alves <douglas.expalves@gmail.com>
2017-12-29 02:54:59 -0200

    remove comments

--- client/main.js
+++ client/main.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import App from '../imports/ui/App'
-// import Login from '../imports/ui/Login'
 
 Meteor.startup(() => {
   render(
     <App />
     , document.getElementById('render-app'))
   })
-  // <MuiThemeProvider> </MuiThemeProvider>
   
